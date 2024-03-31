05f2a68df2831f63d833a4e73ee24183e3258140
Pawan Bhandari <zephyrpawan@gmail.com>
2024-03-31 17:51:58 -0500

    print hello

--- src/main/java/org/example/jenkinshandson/JenkinsHandsOnApplication.java
+++ src/main/java/org/example/jenkinshandson/JenkinsHandsOnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsHandsOnApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello!")
 		SpringApplication.run(JenkinsHandsOnApplication.class, args);
 	}
 
