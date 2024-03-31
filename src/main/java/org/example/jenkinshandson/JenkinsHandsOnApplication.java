1371f400f83973a61153d3e8d196313ce209506b
Pawan Bhandari <zephyrpawan@gmail.com>
2024-03-31 16:23:44 -0500

    first commit

---
+++ src/main/java/org/example/jenkinshandson/JenkinsHandsOnApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkinshandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/jenkinshandson/JenkinsHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jenkinshandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
